5752da0a9ce188d2bdd261ce6ec777faf1d7b892
MarioAAT <marioaguilar740@gmail.com>
2023-02-09 23:33:23 +0100

    archivos JavaScript

---
+++ javascript/juego.js
@@ -0,0 +1,7 @@
+
+
+let infoPlayer1 = document.getElementById("player1");
+infoPlayer1.innerHTML = sessionStorage.getItem("Name player 1");
+
+let infoPlayer2 = document.getElementById("player2");
+infoPlayer2.innerHTML = sessionStorage.getItem("Name player 2");
