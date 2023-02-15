53d7dfa478271b428551812e7abf014e92f24dfa
unknown <marioaguilar740@gmail.com>
2023-02-15 18:24:21 +0100

    limitacion de caracteres en imput page-registro

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,35 @@
+
 const savePlayers = () => {
 
     let playerNom1 = document.getElementById("player1");
+    playerNom1.addEventListener('playerNom1',function(){
+        if (this.value.length > 12) 
+        this.value = this.value.slice(0,12); 
+    })
     let nomPlayer1 = playerNom1.value;
-    // console.log(nomPlayer1);
     sessionStorage.setItem("Nombre player 1", nomPlayer1);
 
     let playerNom2 = document.getElementById("player2");
     let nomPlayer2 = playerNom2.value;
-    // console.log(nomPlayer2);
     sessionStorage.setItem("Nombre player 2", nomPlayer2);
 
+    const displace = () => {
+        this.style.marginleft == "40px" ? this.style.marginleft == "0px" : this.style.marginright == "40px"; 
+        }
+        document.getElementById("play").addEventListener("mouseover", displace);
+
     if(nomPlayer1 == "" || nomPlayer2 == "") {
-        alert("No olvides poner tu nombre");
+        
+        
+    displace()
+                
+    // alert("No olvides poner tu nombre");
     } else {
         document.getElementById("play").href="../pages/tictactoe.html";
-    };
+    }
 }
+    
 
 
+    
 
